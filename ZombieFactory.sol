610a484f4e5077ae4fa1e52401201fb2bba3691d
Fabio Santos Alves <fa3io.santos.alves@gmail.com>
2018-01-16 00:57:17 -0200

    Criação de uma estrutura dentro do nosso contrato.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -6,6 +6,15 @@ contract ZombieFactory{
 
 	//Variavel de estado do contrato fica armazenada na blockchain
 	uint dnaDigits = 16;
+	
+	//Inserindo operações
+	uint dnaModulus = 10 ** dnaDigits;
+
+	//Estruturas nos permte criar tipos de dados mais complexos para nossos contratos
+	struct Zombie{
+        string name;
+        uint dna;
+    }
 
 
 }
